2e9a3fe7fe7e04108ba93360406688422a5500ab
ColoThor <db5250@163.com>
2020-01-29 19:50:47 +0800

    使用新方式以解决划动卡顿问题

--- app/src/main/java/com/debin/wechat/snsad/WeChatAdHook.java
+++ app/src/main/java/com/debin/wechat/snsad/WeChatAdHook.java
@@ -12,6 +12,9 @@ import android.view.ViewGroup;
 import com.debin.wechat.snsad.util.LogUtil;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -73,6 +76,11 @@ public class WeChatAdHook {
 
                                 if (!AD_LINEARLAYOUT_MAP.containsKey(versionCode)) {
                                     LogUtil.e(TAG, "不支持此版本微信");
+                                    return;
+                                }
+
+                                if (versionCode == 1545){
+                                    newHideAD(context.getClassLoader(),BASEADAPTER_CLASS_NAME_MAP.get(versionCode));
                                     return;
                                 }
 
@@ -140,6 +148,88 @@ public class WeChatAdHook {
             }
         } catch (Throwable e) {
             LogUtil.e(TAG, "hideAD err:" + Log.getStackTraceString(e));
+        }
+    }
+
+    private ArrayList<Integer> indexList = new ArrayList<>();
+
+    private void newHideAD(ClassLoader classLoader, String baseAdapterClassName) {
+        try {
+            String listItemClassName = "com.tencent.mm.plugin.sns.ui.ba";
+            Class listItemClazz = classLoader.loadClass(listItemClassName);
+            final Field adTagField = XposedHelpers.findField(listItemClazz, "tKd");
+
+            String storageClassName = "com.tencent.mm.vending.base.b";
+            Class storageClass = classLoader.loadClass(storageClassName);
+            if (storageClass != null) {
+                final Field listSizeFiled = XposedHelpers.findField(storageClass, "c");
+                final Method itemDataMethod = XposedHelpers.findMethodExact(storageClass, "get",
+                        int.class);
+                XposedHelpers.findAndHookMethod(storageClass, "synchronizing", int.class,
+                        Object.class, new XC_MethodHook() {
+                            @Override
+                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                super.afterHookedMethod(param);
+                                indexList.clear();
+                                int size = (int) listSizeFiled.get(param.thisObject);
+                                if (size == 0) {
+                                    return;
+                                }
+
+                                for (int i = 0; i < size; i++) {
+                                    Object item = itemDataMethod.invoke(param.thisObject, i);
+                                    if (item != null) {
+                                        boolean isAd = adTagField.getBoolean(item);
+                                        if (!isAd) {
+                                            indexList.add(i);
+                                        } else {
+                                            LogUtil.e(TAG, "position: " + i + " is ad" );
+                                        }
+                                    }
+                                }
+                                listSizeFiled.set(param.thisObject, indexList.size());
+                                Log.d(TAG, "reset listSize : " + size + " to " + indexList.size());
+                            }
+                        });
+            } else {
+                LogUtil.e(TAG, "class " + storageClassName + " not found");
+                return;
+            }
+
+            final Class clazz = classLoader.loadClass(baseAdapterClassName);
+            if (clazz != null) {
+                XposedHelpers.findAndHookMethod(clazz, "getItem", int.class,
+                        new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                super.beforeHookedMethod(param);
+                                param.args[0] = indexList.get((int) param.args[0]);
+                            }
+                        });
+
+                XposedHelpers.findAndHookMethod(clazz, "getItemViewType", int.class,
+                        new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                super.beforeHookedMethod(param);
+                                param.args[0] = indexList.get((int) param.args[0]);
+                            }
+                        });
+
+                XposedHelpers.findAndHookMethod(clazz, "getView", int.class, View.class,
+                        ViewGroup.class,
+                        new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                super.beforeHookedMethod(param);
+                                param.args[0] = indexList.get((int) param.args[0]);
+                            }
+                        });
+            } else {
+                LogUtil.e(TAG, "class " + baseAdapterClassName + " not found");
+            }
+        } catch (Throwable e) {
+            LogUtil.e(TAG, "newHideAD err:" + Log.getStackTraceString(e));
         }
     }
 }
